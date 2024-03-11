4c624d77bc75e9dcd048611f08ed4dc9dfe571f5
21libr <21libr@stockholmscience.se>
2024-03-11 14:11:32 +0100

    Map Generation

--- Assets/MapGen/MapGeneration.cs
+++ Assets/MapGen/MapGeneration.cs
@@ -5,18 +5,40 @@ using UnityEngine;
 public class MapGeneration : MonoBehaviour
 {
 
-    GameObject[] Tilemaps;
-    public int ChunksSpawned = 5;
+    GameObject Grid;
+    public int chunksSpawned = 5;
+    int selectedMap;
+    List<int> PreviousMaps = new List<int>();
 
     // Start is called before the first frame update
     void Start()
     {
-        Tilemaps = GameObject.FindGameObjectsWithTag("GeneratedMap");
-        System.Random random = new System.Random();
-        for(int i = 0; i < ChunksSpawned; i++)
+        Grid = GameObject.FindGameObjectWithTag("GeneratedMapGrid");
+        int mapcount = Grid.transform.childCount;
+        if(chunksSpawned > mapcount)
         {
-            int selectedMap = random.Next(0, Tilemaps.Length-1);
-            Tilemaps[selectedMap].gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            Debug.Log("Tried to spawn more chunks than there are maps, changed chunks spawned to match mapcount");
+            chunksSpawned = mapcount;
+        }
+        System.Random random = new System.Random();
+        for(int i = 0; i < chunksSpawned; i++)
+        {
+            do
+            {
+                selectedMap = Random.Range(0, mapcount);
+            } while (PreviousMaps.Contains(selectedMap) && PreviousMaps.Count < chunksSpawned);
+
+
+           /* int selectedMap = random.Next(0, mapcount);
+            Debug.Log(selectedMap + " " + i);
+            if(PreviousMaps.Contains(selectedMap) && PreviousMaps.Count < 3)
+            {
+                i--;
+                continue;
+            } */
+            Grid.gameObject.transform.GetChild(selectedMap).gameObject.SetActive(true);
+            Grid.gameObject.transform.GetChild(selectedMap).transform.position = new Vector3(20*1.3f*i,0,0);
+            PreviousMaps.Add(selectedMap);
         }
     }
 
